2108e3543b2bcbef08be5c5f2880fe5ef8bb2608
GreyLee-Hacker <2829604235@qq.com>
2025-02-19 11:56:16 +0800

    2023202254+function3_2

--- 2023202254.c
+++ 2023202254.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
-	printf("This is the first lab!\\n");
-    	printf("This is the second lab!\n");
- 	return 0;
+    printf("This is the first lab!\n");
+    printf("This is the second lab!\n");
+    printf("Lab3 is here!\n");
+    return 0;
 }
